fd167a36dc1c622a210dc7f7a7be4768686b2db5
Raine Pretorius <raine.pretorius1@gmail.com>
2022-12-22 09:20:38 +0200

    first commit

---
+++ js/HeroBanner.js
@@ -0,0 +1,48 @@
+﻿window.addEventListener('DOMContentLoaded', function () {
+    resizeHeroBanner();
+    window.addEventListener('resize', resizeHeroBanner);
+});
+
+function resizeHeroBanner() {
+    var contentWrapperInnerPaddingTop = parseFloat(getElementsComputedStyleProperty('.content-wrapper .content-inner > div', 'padding-top'));
+    var headerHeight = parseFloat(getElementsComputedStyleProperty('.dynamicheadercontainer', 'height'));
+    var heroInnerConentHeight = parseFloat(getElementsComputedStyleProperty('.hero-section.banner .hero-content-height .hero-content-inner-height', 'height'));
+    var heroInnerConentHeight1 = parseFloat(getElementsComputedStyleProperty('.hero-section.hero-layout3 .hero-content-height .hero-content-inner-height', 'height'));
+    var heroInnerConentHeight2 = parseFloat(getElementsComputedStyleProperty('.hero-section.hero-layout4 .hero-content-height .hero-content-inner-height', 'height'));
+    if (Number.isNaN(headerHeight)) {
+        headerHeight = 200;
+    }
+
+    document.querySelectorAll('.hero-section.banner .hero-content-height').forEach(function (elm) {
+        elm.style.height = window.innerHeight - contentWrapperInnerPaddingTop - headerHeight + 'px';
+        elm.style.minHeight = heroInnerConentHeight + 'px';
+    });
+
+    
+    document.querySelectorAll('.hero-section.hero-layout3 .hero-content-height').forEach(function (elm) {
+        if (window.innerWidth > 768) {
+            elm.style.height = window.innerHeight - contentWrapperInnerPaddingTop - headerHeight + 'px';
+            elm.style.minHeight = heroInnerConentHeight1 + 'px';
+        }
+        else {
+            elm.style.height = null;
+            elm.style.minHeight = null;
+        }
+    });
+
+    document.querySelectorAll('.hero-section.hero-layout4 .hero-content-height').forEach(function (elm) {
+        if (window.innerWidth > 768) {
+            elm.style.height = window.innerHeight - contentWrapperInnerPaddingTop - headerHeight + 'px';
+            elm.style.minHeight = heroInnerConentHeight2 + 'px';
+        }
+        else {
+            elm.style.height = null;
+            elm.style.minHeight = null;
+        }
+    });
+}
+
+function getElementsComputedStyleProperty(selector, propertyName) {
+    var element = document.querySelector(selector);
+    return element ? window.getComputedStyle(element).getPropertyValue(propertyName) : null;
+}
---
+++ onewebstatic/google-fonts.php
@@ -0,0 +1,89 @@
+<?php
+
+$url = $_GET['url'];
+
+if (!isset($url)) {
+    http_response_code(400);
+    die('Error: Missing "url" parameter');
+}
+
+if (
+    strpos($url, 'https://fonts.googleapis.com/css?') !== 0 &&
+    strpos($url, 'https://fonts.gstatic.com/'       ) !== 0
+) {
+    http_response_code(400);
+    die('Error: Invalid "url" parameter');
+}
+
+getFile($url);
+
+function getFile($url) {
+    $extensionFromUrl = '';
+    if (strpos($url, 'https://fonts.googleapis.com/css?') === 0) {
+        $extensionFromUrl = 'css';
+    } else {
+        try {
+            $extensionFromUrl = pathinfo(parse_url($url)['path'])['extension'];
+        } catch (Exception $e) {
+            // do nothing
+        }
+    }
+
+    $encoded_url = base64_encode($url);
+    $filename = md5($encoded_url) . '.cache';
+    if ($extensionFromUrl === 'css' || $extensionFromUrl === 'woff2') {
+        $filename = $filename . '.' . $extensionFromUrl;
+    } else {
+        http_response_code(400);
+        die('Error: Invalid "url" parameter ' . $url);
+    }
+    if (!file_exists('./.fonts-cache/' . $filename)) {
+        cacheFile($url, './.fonts-cache/' . $filename);
+    }
+
+    if ($extensionFromUrl === 'css') {
+        header('Content-type: text/css');
+    } else if ($extensionFromUrl === 'woff2') {
+        header('Content-type: font/woff2');
+    }
+    echo file_get_contents('./.fonts-cache/' . $filename);
+}
+
+function cacheFile($url, $filepath) {
+    if (
+        strpos($url, 'https://fonts.googleapis.com/css?') === 0 ||
+        strpos($url, 'https://fonts.gstatic.com/'       ) === 0
+    ) {
+        $content = file_get_contents(
+            $url,
+            false,
+            stream_context_create([
+                'http' => [
+                    'method' => 'GET',
+                    'header' => 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+                ]
+            ])
+        );
+
+        if (!isset($content) || strlen($content) === 0) {
+            http_response_code(500);
+            die('Error: Unable to fetch data');
+        }
+
+        $selfUrl = $_SERVER['SCRIPT_NAME'];
+        $content = str_replace('https://', $selfUrl . '?url=https://', $content);
+
+        if (!is_dir('./.fonts-cache')) {
+            mkdir('./.fonts-cache');
+        }
+
+        file_put_contents($filepath, $content);
+    } else {
+        http_response_code(400);
+        die('Error: Invalid "url" parameter ' . $url);
+    }
+}
+
+exit(0);
+
+?>
